e63fed86a8194ad52dad2cd9a3f7a051cfd088c3
Kei Iwasaki <me@laughk.org>
2016-02-27 16:33:37 +0900

    part2 まで終わり。
    インタラクティブシェルの作業がおおいのでそれほど書いた量は多くはない

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,3 +2,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at polls index.")
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question {0}.".format(question_id))
