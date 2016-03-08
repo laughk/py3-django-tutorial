e36f4346683d6c68350a99ece86225b84861fb3b
Kei Iwasaki <iwasaki@pepabo.com>
2016-03-08 20:46:09 +0900

    チュートリアル3、実際に動作するビューを書く までおわり

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,26 @@
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Question
 
 def index(request):
-    return HttpResponse("Hello, world. You're at polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
+
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question {0}.".format(question_id))
+
+
+def results(request, question_id):
+    response = "You're looking at results of question {0}"
+    return HttpResponse(response.format(question_id))
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question {0}.".format(question_id))
+
