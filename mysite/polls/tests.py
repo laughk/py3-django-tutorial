9e3959b7577b248b8d692da8f5050d4e5e9ace5e
Kei Iwasaki <iwasaki@pepabo.com>
2016-04-05 20:23:17 +0900

    add test / 1日以上前の質問は最近の質問ではないとするべき

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -17,3 +17,14 @@ class QuestionMethodTests(TestCase):
         time = timezone.now() + datetime.timedelta(days=30)
         future_question = Question(pub_date=time)
         self.assertEqual(future_question.was_published_recently(), False)
+
+
+    def test_was_published_recently_with_old_question(self):
+        """
+        was_published_recently() should return False for questions whose
+        pub_date is older than 1day.
+        """
+        time = timezone.now() - datetime.timedelta(days=30)
+        old_question = Question(pub_date=time)
+        self.assertEqual(old_question.was_published_recently(), False)
+
