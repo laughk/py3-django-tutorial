420064ba2d44cd60365db646c0fcfd6c8056cf00
Kei Iwasaki <iwasaki@pepabo.com>
2016-04-05 20:26:14 +0900

    add test / 1日以内の質問は最近の質問とするべき

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -28,3 +28,11 @@ class QuestionMethodTests(TestCase):
         old_question = Question(pub_date=time)
         self.assertEqual(old_question.was_published_recently(), False)
 
+    def test_was_published_recently_with_recent_question(self):
+        """
+        was_published_recently() should return True for questions whose
+        pub_date is within the last day.
+        """
+        time = timezone.now() - datetime.timedelta(hours=1)
+        recent_question = Question(pub_date=time)
+        self.assertEqual(recent_question.was_published_recently(), True)
